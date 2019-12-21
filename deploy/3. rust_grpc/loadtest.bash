7c0b205d71f2c9f4556b267dc2693e42497a7927
steadylearner <32325099+steadylearner@users.noreply.github.com>
2019-12-20 19:26:46 -0500

    Include the loadtest results in loadtest.md and end the work for today. Should make models and separate test and functions for hash and verify to separate folders.

--- deploy/3. rust_grpc/loadtest.bash
+++ deploy/3. rust_grpc/loadtest.bash
@@ -4,4 +4,7 @@
 # sudo apt install npm
 sudo npm install -g loadtest
 # Test the GET API
-loadtest http://0.0.0.0:8000/api/user/v1/steadylearner -t 20 -c 10 --keepalive --rps 2000 
+loadtest http://0.0.0.0:8000/api/user/v1/steadylearner -t 20 -c 10 --keepalive --rps 2000
+# thread 'tokio-runtime-worker' panicked at 'called `Result::unwrap()` on an `Err` value: Error(Client, hyper::Error(Connect, ConnectError("tcp connect error", Os { code: 24, kind: Other, message: "Too many open files" })))', src/libcore/result.rs:1189:5
+
+ 

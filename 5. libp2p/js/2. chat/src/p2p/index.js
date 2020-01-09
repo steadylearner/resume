08b7d078920a6ff3bb2f14bc8181583ae503fde2
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-09 17:01:42 -0500

    Organize the libp2p JavaScript example.

--- 5. libp2p/js/2. chat/src/p2p/index.js
+++ 5. libp2p/js/2. chat/src/p2p/index.js
@@ -1,16 +1,14 @@
-const Libp2p = require('libp2p')
-const Ping = require('libp2p/src/ping')
-
-const defaultsDeep = require('@nodeutils/defaults-deep')
+const Libp2p = require("libp2p");
+const defaultsDeep = require("@nodeutils/defaults-deep");
 
 const { DEFAULT_OPTS } = require("./options");
 
 class P2PNode extends Libp2p {
-  constructor (opts) {
-    super(defaultsDeep(opts, DEFAULT_OPTS))
-  }
+	constructor (opts) {
+		super(defaultsDeep(opts, DEFAULT_OPTS));
+	}
 
 }
 
-module.exports = { P2PNode }
+module.exports = { P2PNode };
 

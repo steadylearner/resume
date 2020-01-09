1a29191565fe74d3571a5a9ef96c7daee38643c6
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-09 05:31:42 -0500

    Modulize the libp2p JavaScript example. Should follow another example of it.

---
+++ 5. libp2p/README.md
@@ -0,0 +1,3 @@
+# How to learn it 
+
+Start with [the JavaScript example from the official website.](https://docs.libp2p.io/tutorials/getting-started/javascript/)
--- 5. libp2p/js/1. get-started/src/p2p/index.js
+++ 5. libp2p/js/1. get-started/src/p2p/index.js
@@ -1,25 +1,9 @@
 const Libp2p = require('libp2p')
-const TCP = require('libp2p-tcp')
-const Multiplex = require('libp2p-mplex')
-const SECIO = require('libp2p-secio')
+const Ping = require('libp2p/src/ping')
 
 const defaultsDeep = require('@nodeutils/defaults-deep')
 
-const Ping = require('libp2p/src/ping')
-
-const DEFAULT_OPTS = {
-  modules: {
-    transport: [
-      TCP
-    ],
-    connEncryption: [
-      SECIO
-    ],
-    streamMuxer: [
-      Multiplex
-    ],
-  }
-}
+const { DEFAULT_OPTS } = require("./options");
 
 class P2PNode extends Libp2p {
   constructor (opts) {
@@ -28,11 +12,13 @@ class P2PNode extends Libp2p {
 
   ping (remotePeerInfo, callback) {
     const p = new Ping(this._switch, remotePeerInfo)
+
     p.on('ping', time => {
       p.stop() // stop sending pings
       callback(null, time)
     })
     p.on('error', callback)
+
     p.start()
   }
 }
